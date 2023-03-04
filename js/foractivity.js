29dc42748a0bf17c355b460e073a3d84aaa41509
kajavick <svkajan@gmail.com>
2023-03-04 09:36:17 +0530

    no message

--- js/foractivity.js
+++ js/foractivity.js
@@ -1,1 +1,23 @@
-var uni;
+var university = "AMDT";
+console.log(university);
+
+var friends = ["friend1","friend2","friend3"];
+console.log(friends[2]);
+
+if (friends[2].startsWith("C")) {
+    console.log("Yes");
+} 
+else {
+    console.log("Nope");
+}
+
+friends = [];
+
+friends.push("Cyril");
+
+if (friends[2].startsWith("C")) {
+    console.log("Yes");
+} 
+else {
+    console.log("Nope");
+}
